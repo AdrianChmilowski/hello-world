88bf75b6846ed35ada17cfca94e24317eab63dbe
achmi <achmi@DESKTOP-77HBCNH>
2019-09-11 14:43:56 +0200

    ver.1

---
+++ Test Suites/New Test Suite1.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0b8131cf-df7b-4a64-b0e0-1c226c0f6300</testSuiteGuid>
+</TestSuiteEntity>
